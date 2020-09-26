7e05dea86b253e3a26768a1224e6430e4877407c
vikash212000yadav <vikash212000yadav@gmail.com>
2020-09-26 15:10:31 +0530

    v0.0

--- comparison/forms.py
+++ comparison/forms.py
@@ -4,13 +4,20 @@ from django import forms
 class FilterForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
-        choices_company = kwargs.pop('choices_company')
-        choices_product = kwargs.pop('choices_product')
-        choices_parameter = kwargs.pop('choices_parameter')
+        # choices_company = kwargs.pop('choices_company')
+
+        choices_chipset = kwargs.pop('choices_chipset')
+        choices_benchmark = kwargs.pop('choices_benchmark')
         super(FilterForm, self).__init__(*args, **kwargs)
-        self.fields["Company"] = forms.MultipleChoiceField(choices=choices_company,
-                                                           widget=forms.CheckboxSelectMultiple(), required=True)
-        self.fields["Product"] = forms.MultipleChoiceField(choices=choices_product,
-                                                           widget=forms.CheckboxSelectMultiple(), required=True)
-        self.fields["Parameter"] = forms.MultipleChoiceField(choices=choices_parameter,
-                                                           widget=forms.CheckboxSelectMultiple(), required=True)
+
+        # self.fields["Company"] = forms.MultipleChoiceField(choices=choices_company, widget=forms.CheckboxSelectMultiple(), required=True)
+
+        # self.fields["Chipset"] = forms.MultipleChoiceField(choices=choices_chipset,
+        #                                                   widget=forms.CheckboxSelectMultiple(), required=True)
+        # self.fields["Benchmark"] = forms.MultipleChoiceField(choices=choices_benchmark,
+        #                                                     widget=forms.CheckboxSelectMultiple(), required=True)
+
+        self.fields["Chipset"] = forms.MultipleChoiceField(choices=choices_chipset,
+                                                           widget=forms.SelectMultiple(), required=True)
+        self.fields["Benchmark"] = forms.MultipleChoiceField(choices=choices_benchmark,
+                                                             widget=forms.SelectMultiple(), required=True)
--- comparison/views.py
+++ comparison/views.py
@@ -15,23 +15,23 @@ from django.views import generic
 
 def home(request):
     db_choices = FilterChoices.objects.all()[0]
-    if (request.method == "GET"):
+    if request.method == "GET":
         jsonDec = json.decoder.JSONDecoder()
-        choices_company = [(i, i) for i in jsonDec.decode(db_choices.company)]
-        choices_product = [(i, i) for i in jsonDec.decode(db_choices.product)]
-        choices_parameter = [(i, i) for i in jsonDec.decode(db_choices.parameter)]
+        # choices_company = [(i, i) for i in jsonDec.decode(db_choices.company)]
+        choices_chipset = [(i, i) for i in jsonDec.decode(db_choices.chipset)]
+        choices_benchmark = [(i, i) for i in jsonDec.decode(db_choices.benchmark)]
 
-        form = FilterForm(choices_company=choices_company, choices_product=choices_product,
-                          choices_parameter=choices_parameter)
+        form = FilterForm(choices_chipset=choices_chipset,
+                          choices_benchmark=choices_benchmark)
 
         return render(request, "comparison/home.html", {'form': form})
 
     if request.method == "POST" and 'submit' in request.POST:
-        form_company = request.POST["Company"] or None
-        form_product = request.POST["Product"] or None
-        form_parameter = request.POST["Parameter"] or None
+        # form_company = request.POST["Company"] or None
+        form_chipset = request.POST["Chipset"] or None
+        form_benchmark = request.POST["Benchmark"] or None
         return render(request, "comparison/comparison.html",
-                      {'form_company': form_company, 'form_product': form_product, 'form_parameter': form_parameter})
+                      {'form_chipset': form_chipset, 'form_benchmark': form_benchmark})
 
 
 def compare(request):
