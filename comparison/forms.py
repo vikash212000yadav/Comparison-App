e122ef678ad48d8a9469614ef7270337925ea555
vikash212000yadav <vikash212000yadav@gmail.com>
2020-10-02 00:35:26 +0530

    v1.0

--- comparison/forms.py
+++ comparison/forms.py
@@ -31,20 +31,11 @@ class SignUpForm(UserCreationForm):
 class FilterForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
-        # choices_company = kwargs.pop('choices_company')
-
         choices_chipset = kwargs.pop('choices_chipset')
         choices_benchmark = kwargs.pop('choices_benchmark')
         super(FilterForm, self).__init__(*args, **kwargs)
-
-        # self.fields["Company"] = forms.MultipleChoiceField(choices=choices_company, widget=forms.CheckboxSelectMultiple(), required=True)
 
         self.fields["Chipset"] = forms.MultipleChoiceField(choices=choices_chipset,
                                                            widget=forms.CheckboxSelectMultiple(), required=True)
         self.fields["Benchmark"] = forms.MultipleChoiceField(choices=choices_benchmark,
                                                              widget=forms.CheckboxSelectMultiple(), required=True)
-
-        # self.fields["Chipset"] = forms.MultipleChoiceField(choices=choices_chipset,
-        #                                                   widget=forms.SelectMultiple(), required=True)
-        # self.fields["Benchmark"] = forms.MultipleChoiceField(choices=choices_benchmark,
-        #                                                     widget=forms.SelectMultiple(), required=True)
--- comparison/models.py
+++ comparison/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-"""
-class FilterChoices(models.Model):
-    # company = models.TextField(null=True, verbose_name="Company", )
-    chipset = models.TextField(null=True, verbose_name="Product", )
-    benchmark = models.TextField(null=True, verbose_name="Parameter", )
-    values = models.TextField(null=True, verbose_name="Values", )
-"""
-
 
 class Chipset(models.Model):
     chipset_name = models.TextField(null=True, verbose_name="Product", )
@@ -30,16 +22,3 @@ class FilterValue(models.Model):
 
     def __str__(self):
         return '%s %s' % (str(self.chipset), str(self.benchmark))
-
-
-#     def __str__(self):
- #        return str(self.values)
-
-"""
-    Chipset
-B1    1
-B2    2
-B3    3
-B4    4
-B5    5
-"""
