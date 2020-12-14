65f69815928863a39aac3bb8e087b0c1f10086d3
vikash212000yadav <vikash212000yadav@gmail.com>
2020-12-14 15:32:07 +0530

    styling updated

--- comparison/admin.py
+++ comparison/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from comparison.models import Chipset, Benchmark, FilterValue
 from import_export.admin import ImportExportModelAdmin
 
 
--- comparison/views.py
+++ comparison/views.py
@@ -1,15 +1,13 @@
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView
 
-from .forms import *
-from .models import *
+from comparison.forms import EditProfileForm, SignUpForm, FilterForm
+from comparison.models import Chipset, Benchmark, FilterValue
 import json
 from . import models
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
 
 
 # Create your views here.
